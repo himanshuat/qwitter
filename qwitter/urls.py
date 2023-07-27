66ea416eceedf81d6c458ded81ab217067453c71
himanshuat <himanshureddy0846@gmail.com>
2023-07-27 12:57:04 +0530

    Get production ready

--- qwitter/urls.py
+++ qwitter/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("network.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
