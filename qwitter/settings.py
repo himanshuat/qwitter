248f0403fe6474702005c9a0ee6f5c89cd8f6e4d
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2023-08-08 19:43:12 +0530

    ✨ Customize Navbar

--- qwitter/settings.py
+++ qwitter/settings.py
@@ -85,6 +85,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 AUTH_USER_MODEL = "network.User"
 
 # Password validation
