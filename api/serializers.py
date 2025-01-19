72a6d2729cae5405b5e6af12438d49ca1d68ab44
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2025-01-19 16:18:17 +0530

    Add new fields to post serializer

--- api/serializers.py
+++ api/serializers.py
@@ -4,11 +4,13 @@ from network.models import *
 
 class PostSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
+    liked = serializers.SerializerMethodField()
+    bookmarked = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ["id", "content", "date", "ispinned", "reactions_count", "comments_count", "user"]
-        read_only_fields = ["id", "date", "ispinned", "reactions_count", "comments_count", "user"]
+        fields = ["id", "content", "date", "ispinned", "reactions_count", "comments_count", "user", "liked", "bookmarked"]
+        read_only_fields = ["id", "date", "ispinned", "reactions_count", "comments_count", "user", "liked", "bookmarked"]
 
     def get_user(self, obj):
         user = obj.user
@@ -19,6 +21,18 @@ class PostSerializer(serializers.ModelSerializer):
             "name": user.profile.name if user.profile else None,
             "image": user.profile.image if user.profile and user.profile.image else None,
         }
+    
+    def get_liked(self, obj):
+        if self.context['request'].user.is_authenticated:
+            result = Reaction.objects.filter(post_id=obj.id, user_id=self.context['request'].user.id)
+            return bool(len(result))
+        return False
+    
+    def get_bookmarked(self, obj):
+        if self.context['request'].user.is_authenticated:
+            result = Bookmark.objects.filter(post_id=obj.id, user_id=self.context['request'].user.id)
+            return bool(len(result))
+        return False
     
 
 class CommentSerializer(serializers.ModelSerializer):
