bd8512c564c5699945b84d4849871318243d8377
Himanshu Tegyalwar <83063151+himanshuat@users.noreply.github.com>
2025-11-10 00:10:32 +0530

    feat(ui): integrate Django humanize for relative time display

--- qwitter/settings/base.py
+++ qwitter/settings/base.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # Third-party
     "rest_framework",
     "drf_spectacular",
